b003ec9e798d9d6d97f54f0ce68f59b102493139
e-colot <emmeran.colot@gmail.com>
2025-11-10 07:27:52 +0100

    Impose a 1V rms

--- scripts/excitation.m
+++ scripts/excitation.m
@@ -4,9 +4,10 @@ clear; close all; clc;
 
 %% RMS + band of interest determination
 
-N = 50e3;           % Number of samples
+N = 50e3;
 
 randInput = randn(N,1);
+randInput = randInput / rms(randInput);   % impose an rms of 1V
 
 save('../excitation signals/randInput.mat','randInput');
 
